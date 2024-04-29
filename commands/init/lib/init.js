0e5b83dff2bbb2931c3eefd5ea19f9a96d988ff5
zhb333 <1140457303@qq.com>
2024-04-30 00:25:24 +0800

    chore: 项目初始化

--- commands/init/lib/init.js
+++ commands/init/lib/init.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const fs = require("fs");
 const Command = require("@zhb-cli/command");
 const log = require("@zhb-cli/log");
 
@@ -13,7 +14,33 @@ class InitCommand extends Command {
   }
 
   exec() {
-    console.log('init的业务逻辑');
+    try {
+      //  1.准备阶段
+      this.prepare();
+    } catch (e) {
+      log.error(e.message);
+    }
+  }
+
+  prepare() {
+    // 1. 判断当前目录是否为空
+    if (!his.isCwdEmpty()) {
+      // 询问是否继续创建
+    }
+
+    // 2. 是否启动强制更新
+    // 3. 选择创建项目或者组件
+    // 4. 获取项目基本信息
+  }
+
+  isCwdEmpty() {
+    const localPath = process.cwd();
+    let fileList = fs.readdirSync(localPath);
+    // 文件过滤的逻辑
+    fileList = fileList.filter(file => {
+      return !file.startsWith('.') && ['node_modules'].indexOf(file) < 0
+    })
+    return !fileList || fileList.length <= 0
   }
 }
 
